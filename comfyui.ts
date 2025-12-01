91a2e79f5457bcc959fb52b59b8e6cc1e63badc1
drake7707 <sarah.kerkhove@ucll.be>
2025-12-01 09:20:02 +0100

    squash

---
+++ comfyui.ts
@@ -0,0 +1,262 @@
+
+export interface PromptEntry {
+    id: string;
+    status: "queued" | "executing" | "finished";
+    onNodeExecuting: (nodeId: string) => void;
+    onNodeExecuted: (nodeId: string, output: any) => void
+    onStart: () => void
+    onFinish: () => void;
+    outputs: { [key: string]: any }
+
+    activeNode: string|null;
+    nodesExecuted: string[];
+    nodesToExecute: string[];
+    progress: number;
+}
+
+export interface UploadResult {
+    name: string;
+    subfolder: string;
+    type: string;
+}
+
+export class ComfyUI {
+
+
+    private registeredPrompts: { [key: string]: PromptEntry } = {};
+
+    private ws: WebSocket | null = null;
+
+    private monitoringData: any;
+
+    constructor(private clientId:string, private server: string) {
+
+    }
+
+    async connect() {
+        if (this.connected) {
+            throw new Error("Already connected");
+        }
+
+        const promise = new Promise((resolve, error) => {
+            this.ws = new WebSocket(`ws://${this.server}/ws?clientId=${this.clientId}`);
+
+
+            this.ws.onopen = async () => {
+                //console.log("Connected to websocket");
+                resolve(true);
+            };
+
+            this.ws.onclose = async () => {
+                //console.log("Disconnected from websocket");
+                this.ws = null;
+            }
+
+            this.ws.onerror = (ev) => {
+                //console.error("Websocket connection failed");
+                error(ev);
+            };
+
+            this.ws.onmessage = async (ev) => {
+                if (typeof ev.data == "string") {
+                    const msg = JSON.parse(ev.data);
+                    if (msg.type == "execution_start") {
+                        const status = this.registeredPrompts[msg.data.prompt_id];
+                        if (typeof status !== "undefined") {
+                            status.onStart();
+                        }
+                    }
+                    else if (msg.type == "execution_cached") {
+                        const status = this.registeredPrompts[msg.data.prompt_id];
+                        if (typeof status !== "undefined") {
+                            for (let node of msg.data.nodes) {
+                                status.onNodeExecuted(node, undefined);
+                            }
+                        }
+                    }
+                    else if (msg.type == "executing") {
+                        //console.log(ev.data);
+                        const status = this.registeredPrompts[msg.data.prompt_id];
+                        if (typeof status !== "undefined") {
+                            if (msg.data.node != null) {
+                                status.onNodeExecuting(msg.data.node);
+                            } else {
+                                status.onFinish();
+                            }
+                        }
+                    }
+                    else if (msg.type == "executed") {
+                        //console.log(ev.data);
+                        const status = this.registeredPrompts[msg.data.prompt_id];
+                        if (typeof status !== "undefined") {
+                            if (msg.data.node != null) {
+                                status.onNodeExecuted(msg.data.node, msg.data.output);
+                            }
+                        }
+                    } else if (msg.type == "execution_success") {
+                        const status = this.registeredPrompts[msg.data.prompt_id];
+                        if (typeof status !== "undefined") {
+                            status.onFinish();
+                        }
+                    }
+                    else if (msg.type == "crystools.monitor") {
+                        this.monitoringData = msg.data;
+                    } else {
+                        //console.log(ev.data);
+                    }
+                } else {
+                    // raw data
+                }
+            };
+
+        });
+    }
+
+    get connected() {
+        return this.ws != null && this.ws.readyState == WebSocket.OPEN;
+    }
+
+    disconnect() {
+        if(this.ws != null) {
+            this.ws.close();
+            this.ws = null;
+        }
+    }
+
+    getMonitoringData() {
+        return this.monitoringData;
+    }
+
+    resolveOutputData(output:any) {
+        const result = [];
+
+        if (typeof output.images !== "undefined") {
+            for (let img of output.images) {
+                const params = new URLSearchParams();
+                params.set("filename", img.filename);
+                params.set("subfolder", img.subfolder);
+                params.set("type", img.type);
+                result.push({
+                    type: "image",
+                    src: `http://${this.server}/view?` + params.toString()
+                })
+            }
+            return result;
+        } else if (typeof output.text !== "undefined") {
+            return [{
+                "type": "text",
+                "text": (<string[]>output.text).join("")
+            }];
+        }
+
+        return null;
+    }
+
+    async queue(prompt: any, onUpdate: (entry:PromptEntry) => void) {
+        const result = await fetch(`http://${this.server}/prompt`, <any>{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                client_id: this.clientId
+            })
+        });
+        const data = await result.json();
+
+
+        const promiseResult = new Promise<PromptEntry>((onResolve, onError) => {
+            //console.log(data);
+            if (typeof data.error !== "undefined") {
+                onError(data);
+                return;
+            }
+
+            if (result.status != 200) {
+                onError(result.statusText);
+                return;
+            }
+
+
+            const promptId = data['prompt_id'];
+
+            //console.log(`Request registered with prompt id ${promptId}`);
+
+            const outputs:any = {};
+            const entry: PromptEntry = {
+                id: promptId,
+                status: 'queued',
+
+                onStart: () => {
+                    entry.status = "executing";
+                    onUpdate(entry);
+                },
+                onNodeExecuting: (nodeId) => {
+                    entry.status = "executing";
+                    
+                    entry.activeNode = nodeId
+
+                    entry.nodesExecuted.push(nodeId);
+                    entry.nodesToExecute.splice(entry.nodesToExecute.indexOf(nodeId),1);
+                    entry.progress = entry.nodesExecuted.length / (entry.nodesExecuted.length + entry.nodesToExecute.length);
+
+                    onUpdate(entry);
+                },
+                onNodeExecuted: (nodeId, output) => {
+                    if (typeof output !== "undefined")
+                        outputs[nodeId] = output;
+
+                    onUpdate(entry);
+                },
+                onFinish: () => {
+
+                    entry.status = "finished";
+                    onResolve(entry);
+                    delete this.registeredPrompts[promptId];
+                },
+                outputs: outputs,
+
+                activeNode: null,
+                nodesToExecute: Object.keys(prompt),
+                nodesExecuted: [],
+                progress: 0
+            }
+            
+            this.registeredPrompts[promptId] = <any>entry;
+
+
+        });
+        return promiseResult;
+    }
+
+
+    async upload(file: File): Promise<UploadResult> {
+        const formData = new FormData();
+        formData.append("image", file);
+        //formData.append("subfolder", "connector_uploads");
+       
+        try {
+            const response = await fetch(`http://${this.server}/api/upload/image`, {
+                method: "POST",
+                body: formData,
+            });
+
+            if (response.status === 200) {
+                // {"name": "vlaams_snippet.mp3", "subfolder": "", "type": "input"}
+                const data = await response.json();
+                return data;
+            } else {
+                throw new Error(`Upload failed with status ${response.status}`);
+            }
+
+        }
+        catch (err) {
+            console.error("Error during upload: " + err);
+            throw err;
+        }
+    }
+
+}
+
---
+++ main.js
@@ -0,0 +1,127 @@
+import { readFile } from "fs/promises";
+import { ComfyUI } from "./comfyui.js";
+import { randomUUID } from "crypto";
+import * as express from 'express';
+const app = express();
+app.use(express.json());
+app.put('/upload', async (req, res) => {
+    const comfyui = new ComfyUI("test", server);
+    await comfyui.upload();
+    comfyui.upload(req.body.file).then((uploadResult) => {
+        ;
+    }, app.listen(8190, () => {
+    }));
+    const server = "172.20.50.213:8189";
+    const comfyui = new ComfyUI("test", server);
+    const generatePromptTemplate = {
+        "10": {
+            "inputs": {
+                "model_type": "large-v3",
+                "batch_size": 4,
+                "if_mutiple_speaker": true,
+                "use_auth_token": "",
+                "if_translate": false,
+                "translator": "alibaba",
+                "to_language": "en",
+                "audio": [
+                    "76",
+                    0
+                ]
+            },
+            "class_type": "WhisperX",
+            "_meta": {
+                "title": "WhisperX Node"
+            }
+        },
+        "40": {
+            "inputs": {
+                "srt": [
+                    "10",
+                    0
+                ]
+            },
+            "class_type": "SRTToString",
+            "_meta": {
+                "title": "SRT to String"
+            }
+        },
+        "76": {
+            "inputs": {
+                "audio": "sp_a2_bts5_lockeddoor05.wav",
+                "choose audio file to upload": "Audio"
+            },
+            "class_type": "LoadAudioVideoPath",
+            "_meta": {
+                "title": "LoadAudioVideoPath"
+            }
+        },
+        "78": {
+            "inputs": {
+                "text": "",
+                "anything": [
+                    "40",
+                    0
+                ]
+            },
+            "class_type": "easy showAnything",
+            "_meta": {
+                "title": "Show Any"
+            }
+        }
+    };
+    async function process(generatePromptTemplate, uploadResult) {
+        let prompt = JSON.parse(JSON.stringify(generatePromptTemplate));
+        prompt["76"].inputs.audio = uploadResult.name;
+        if (!comfyui.connected) {
+            console.log("Comfyui not connected, connecting to server");
+            await comfyui.connect();
+        }
+        let status = "";
+        let progress = 0;
+        try {
+            status = "Waiting";
+            const result = await comfyui.queue(prompt, (entry) => {
+                status = entry.status;
+                progress = entry.progress;
+            });
+            console.log("Succesfully executed");
+            status = "";
+            progress = 100;
+            const outputs = [];
+            for (const nodeId of Object.keys(result.outputs)) {
+                const output = result.outputs[nodeId];
+                const resolvedOutput = comfyui.resolveOutputData(output);
+                outputs.push(resolvedOutput);
+            }
+            return outputs;
+        }
+        catch (err) {
+            console.error("Error: " + JSON.stringify(err));
+            throw err;
+        }
+    }
+    async function run() {
+        const filename = randomUUID().toString();
+        const buffer = await readFile("./vlaams_snippet.mp3");
+        const file = new File([buffer], filename + ".mp3", {
+            type: "audio/mpeg"
+        });
+        const uploadResult = await comfyui.upload(file);
+        const resolvedOutput = await process(generatePromptTemplate, uploadResult);
+        for (let o of resolvedOutput) {
+            if (o.type == "image") {
+                //img.src = o.src;
+                console.log("Image URL: " + o.src);
+            }
+            else if (o.type == "text") {
+                //o.text;
+                console.log("Text output: " + o.text);
+            }
+        }
+    }
+    run().then(() => {
+        console.log("Done");
+    }).catch((err) => {
+        console.error("Error in run: " + err);
+    });
+});
